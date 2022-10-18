8af63bfeb1acd7d6dc73e5146c6806142bf53bee
TMWAG <semyon.valyaev@gmail.com>
2022-10-18 23:44:17 +0300

    formated with prettier, uncommented few strings in /server/index.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,11 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+module.exports = new Sequelize(
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWORD}`,
+  {
     host: `${process.env.DB_HOST}`,
-    dialect: `mariadb`
-});
+    dialect: `mariadb`,
+  }
+);
