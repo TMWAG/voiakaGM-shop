22c6688985322db0810c4850a396da87a3b13d3e
TMWAG <semyon.valyaev@gmail.com>
2022-10-20 22:08:30 +0300

    added user input validation

---
+++ server/utility/inputValidation.js
@@ -0,0 +1,30 @@
+module.exports = class InputValidation {
+  static checkFullName(...fullName) {
+    const nameRegExp = /^[а-яА-ЯёЁa-zA-Z]+$/;
+    return fullName.every(
+      (element) => typeof element === "string" && nameRegExp.test(element)
+    );
+  }
+
+  static checkPhone(phone) {
+    const phoneRegExp =
+      /^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/;
+    return phoneRegExp.test(phone);
+  }
+
+  static checkEmail(email) {
+    const emailRegExp = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
+    return emailRegExp.test(email);
+  }
+
+  static checkPassword(password) {
+    const passwordRegExp =
+      /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+    return passwordRegExp.test(password);
+  }
+
+  static checkTgLink(tgLink){
+    const tgLinkRegExp = /^[A-Za-z\d_]{5,32}$/;
+    return tgLinkRegExp.test(tgLink);
+  }
+};
