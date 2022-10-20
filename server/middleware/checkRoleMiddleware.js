14b4a162193815053cc1e59b0fca07dd41cfa50b
TMWAG <semyon.valyaev@gmail.com>
2022-10-20 21:52:15 +0300

    Added middlewares for authorization and role checking

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (roleId) {
+  return function (req, res, next) {
+    if (req.method === "OPTIONS") {
+      next();
+    }
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      if (decoded.roleId !== roleId) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
+  };
+};
