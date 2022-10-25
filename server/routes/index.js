db5ddfd824a4c5fcf88cb8324c16bb02dc5732df
TMWAG <semyon.valyaev@gmail.com>
2022-10-25 23:52:58 +0300

    new controller & router for parameter entity

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const Router = require("express");
 const router = new Router();
 const userRouter = require("./userRouter");
 const roleRouter = require("./roleRouter");
+const parameterRouter = require("./parameterRouter");
 
 router.use("/user", userRouter);
 router.use("/role", roleRouter);
+router.use("/parameter", parameterRouter);
 
 module.exports = router;
---
+++ server/routes/parameterRouter.js
@@ -0,0 +1,15 @@
+const Router = require("express");
+const router = new Router();
+const parameterController = require("../controllers/parameterController");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
+
+router.post("/create", checkRoleMiddleware(process.env.DEFAULT_ADMINISTRATOR_ROLE_ID), parameterController.create);
+
+router.get("/all", parameterController.getAll);
+router.get("/id/:id", parameterController.getOneById);
+
+router.put("/rename", checkRoleMiddleware(process.env.DEFAULT_ADMINISTRATOR_ROLE_ID), parameterController.changeNameById);
+
+router.delete("/delete", checkRoleMiddleware(process.env.DEFAULT_ADMINISTRATOR_ROLE_ID), parameterController.deleteById);
+
+module.exports = router;
